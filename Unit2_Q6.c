e00cefa356f6f63cea8a052098d6a18f7dbbbcf4
Aayush Anshul <99768325+Aayxsh@users.noreply.github.com>
2022-05-27 23:57:11 +0530

    Create Unit2_Q6.c

---
+++ Unit2_Q6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int arr[3]:
+int 1;
+for (i-0;i<3;i++){
+scan ("*d",&arr[i]):
+)
+1f(arr[0]=-arr[1]&&arr[1]=-arr[2]){
+print ("Equilateral Triangle"):
+else if(arr[0]--arr[1]||arr[1]--arr[2]||arr[0]=-arr[2]){
+print ("Isosceles Triangle"):
+)
+elsef
+printf ("Scalene Triangle");
+)
+return 0;}
